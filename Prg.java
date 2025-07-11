e618f49167c3175b84575b9a76975c6a9596dddb
kolla10 <keertikolla.10@gmail.com>
2025-07-11 12:56:48 +0530

    Add files via upload

---
+++ Prg.java
@@ -0,0 +1,17 @@
+class Prg
+{
+public class static voic main(String[] args)
+{
+while
+{
+int sum=0, avg;
+for (i=0;i<=10;i++)
+sum= sum+1;
+avg= sum/10;
+}
+System.out.println("sum"+sum);
+System.out.println("avg"+avg);
+}
+}
+
+
